8c47bdb3a76ff5814e8c0d31f1b2603f552622d3
Paul Menefee <paul.menefee@gmail.com>
2016-06-21 11:48:09 -0400

    init commit
    Base Project

---
+++ Source/SWA/SWAGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SWAGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWA_API ASWAGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
